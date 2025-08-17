2c2ecb77ad1d4ec94dac43d5b65f9b81be1ab52f
ferbertigo <I_Ferbert@gmx.de>
2025-08-17 11:32:49 +0200

    message model

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -1,21 +1,28 @@
-import { Message, IMessage } from "../models/messageModel";
-import { ensureObjectId } from "../utils/objectIdValidator";
+import { Message, IMessage } from '../models/messageModel';
+import { ensureObjectId } from '../utils/objectIdValidator';
 
-export async function getMessagesByChatId(chatIdRaw: string): Promise<IMessage[] | null> {
-const chatId = ensureObjectId(chatIdRaw);
-const messages = await Message.find({ chat: chatId} ).populate("participants", "name").sort({ createdAt: 1 });
-return messages;
+export async function getMessagesByChatId(
+  chatIdRaw: string,
+): Promise<IMessage[] | null> {
+  const chatId = ensureObjectId(chatIdRaw);
+  const messages = await Message.find({ chat: chatId })
+    .populate('participants', 'name')
+    .sort({ createdAt: 1 });
+  return messages;
 }
 
-export async function saveByChatAndAuthorId(chatIdRaw: string, authorIdRow: string, contentRow: string): Promise<IMessage | null> {
+export async function saveByChatAndAuthorId(
+  chatIdRaw: string,
+  authorIdRow: string,
+  contentRow: string,
+): Promise<IMessage | null> {
+  const chatId = ensureObjectId(chatIdRaw);
+  const authorId = ensureObjectId(authorIdRow);
+  const content = contentRow.toString;
 
-const chatId = ensureObjectId(chatIdRaw);
-const authorId = ensureObjectId(authorIdRow);
-const content  = contentRow.toString;
-
-return Message.create({
-      content: content, 
-      chat: chatId,
-      author: authorId,
-    });
+  return Message.create({
+    content: content,
+    chat: chatId,
+    author: authorId,
+  });
 }
